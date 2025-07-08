8a9b1af6b26cff46c598a74e704f0e36c041a3b4
pavelguzenfeld <pavelguzenfeld@gmail.com>
2025-07-08 18:10:15 +0000

    fix: Format run.sh for improved readability and consistency

--- miniman-tensor-flow-docker/run.sh
+++ miniman-tensor-flow-docker/run.sh
@@ -3,14 +3,14 @@
 # Run the container, ensuring GPU access with --runtime nvidia
 # The container will run the test_gpu.py script and then exit.
 sudo docker run \
-    -it \
-    --rm \
-    --runtime nvidia \
-    --network host \
-    --privileged \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -e NVIDIA_VISIBLE_DEVICES=all \
-    -e NVIDIA_DRIVER_CAPABILITIES=compute,utility \
-    -e NVIDIA_REQUIRE_CUDA="cuda>=11.4" \
-    -v /dev:/dev \
-    my-minimal-tf-ubuntu:jp51
+  -it \
+  --rm \
+  --runtime nvidia \
+  --network host \
+  --privileged \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e NVIDIA_VISIBLE_DEVICES=all \
+  -e NVIDIA_DRIVER_CAPABILITIES=compute,utility \
+  -e NVIDIA_REQUIRE_CUDA="cuda>=11.4" \
+  -v /dev:/dev \
+  my-minimal-tf-ubuntu:jp51
